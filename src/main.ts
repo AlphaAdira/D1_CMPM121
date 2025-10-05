f87739ee2dad044aff851d2dc92c9e92d474b40b
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-10-05 16:25:04 -0700

    step 1: make a button turned the image into a button

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Example image asset:</p>
+  <button id="increment"><img src="${exampleIconUrl}" class="icon" /></button>
 `;
