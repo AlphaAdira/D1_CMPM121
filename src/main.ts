79f1292e5d28855be8b9c3fc48faaa5cf3390fbf
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-10-05 16:30:25 -0700

    step 2: created counter that increases when button is pressed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter = 0;
+
 document.body.innerHTML = `
   <p>Example image asset:</p>
+  <p>Boops: <span id="counter">${counter}</span></p>
   <button id="increment"><img src="${exampleIconUrl}" class="icon" /></button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.textContent = counter.toString();
+});
