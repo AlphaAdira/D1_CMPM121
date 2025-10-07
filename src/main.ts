551fc02fe269786e97e783a9f3d4eafc708cbcc9
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-10-07 22:37:53 +0000

    added some style aspects

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,40 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 let counter = 0;
+const t0 = performance.now();
+//const t1 = performance.now();
+console.log(t0);
+
+document.head.innerHTML = `
+  <title>Adira's D1 Project</title>
+  <style>
+    body {
+      font-family: system-ui, sans-serif;
+      text-align: center;
+      margin-top: 5rem;
+    }
+    .icon {
+      width: 100px;
+      height: 100px;
+    }
+    #increment {
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+    #increment:hover {
+      transform: scale(1.1);
+    }
+    #increment:active {
+      transform: scale(0.9);
+    }
+  </style>
+`;
 
 document.body.innerHTML = `
   <h1>Adira's D1 Project</h1>
   <p>Boops: <span id="counter">${counter}</span></p>
-  <button id="increment"><img src="${exampleIconUrl}" class="icon" /></button>
+  <div id="increment"><img src="${exampleIconUrl}" class="icon" /></div>
   `;
 
 // Add click handler
